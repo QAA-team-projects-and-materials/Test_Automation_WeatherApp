f92e9c52d1d1053e9f59d3224340694acec7ef3b
Grudilov24 <116968051+Grudilov24@users.noreply.github.com>
2024-09-18 11:01:34 +0300

    Dmytro test branch (#4)
    * Update LogOutTest.cy.js
    
    * LogIn/Logout update
    
    * Create test.txt

--- cypress/e2e/TestTask/LogOutTest.cy.js
+++ cypress/e2e/TestTask/LogOutTest.cy.js
@@ -1,10 +1,10 @@
-describe('User Login', () => {
+describe('User LogOut', () => {
 
   beforeEach(() => {
    // Loading user data 
     const user = Cypress.env("user")
     
-  // LOG IN
+  // LOG  IN
   // Navigate to the OpenWeather login page.
     cy.visit('https://openweathermap.org')
 
@@ -17,7 +17,7 @@ describe('User Login', () => {
     cy.get('.new_user > .btn').click()
   })
 
-  it('Verify the presence of key elements', () => {
+  it('LogOut test', () => {
   //LOG OUT
     cy.get('.inner-user-container').wait(5000).click()
     cy.get(':nth-child(5) > .logout').click()
